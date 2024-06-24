b2235624710a237c3c2500546f48da1309a05b14
PredatorXxRIPxX <wassimou009@gmail.com>
2024-06-24 03:40:06 +0200

    e-commerce

--- src/app/client/[id]/Orders/page.tsx
+++ src/app/client/[id]/Orders/page.tsx
@@ -1,8 +1,13 @@
-export default function Orders({params}:{params:{id:string}}){
+import prisma from "../../../../db/db"
+import CardUI from "../../../_lib/@Card/CardUI"
+import { orderType } from "../../../_lib/types/type"
+
+export default async function Orders({params}:{params:{id:string}}){
+    const userOrdersFetch:any[] = await prisma.order.findMany({where:{userId:params.id}})
     return (
-        <div>
-            <h1>Orders</h1>
-            <p>{params.id}</p>
+        <div className="p-6 bg-white rounded-lg text-black">
+            <h1 className="font-bold text-black text-3xl">Orders: </h1>
+            {userOrdersFetch.length==0?<h1 className="text-center text-2xl">No Orders</h1>:userOrdersFetch.map((orderUser:any)=><CardUI id={orderUser.id} title={orderUser.title} description={orderUser.description} price={orderUser.price} imageUrl={orderUser.imageUrl}/>)}
         </div>
     )
 }
