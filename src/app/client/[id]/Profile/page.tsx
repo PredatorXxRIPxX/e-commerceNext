f0c933a1729f884b6cb46a5bbb37b97bdc4d2ff0
PredatorXxRIPxX <wassimou009@gmail.com>
2024-06-24 03:23:11 +0200

    e-commerce

--- src/app/client/[id]/Profile/page.tsx
+++ src/app/client/[id]/Profile/page.tsx
@@ -1,8 +1,17 @@
+"use client"
+import { Button } from "@chakra-ui/react";
+import { useSelector } from "react-redux"
 export default function Profile({params}:{params:{id:string}}){
+    const user = useSelector((state:any)=>state.user);
     return (
-        <div>
-            <h1>Profile</h1>
-            <p>{params.id}</p>
+        <div className="p-6 rounded-lg bg-white text-black w-full">
+            <h1 className="font-bold text-3xl">username: {user.username}</h1>
+            <p className="font-bold text-xl text-gray-600"> user ID: {params.id}</p>
+            <p className=" font-semibold text-xl text-black">user Email: {user.email}</p>
+            <div className="flex items-center justify-center w-full">
+                <Button colorScheme="blue" className="self-center text-white bg-blue-500 ml-2 rounded-lg p-5">Edit Profile</Button>
+                <Button colorScheme="red" className="self-center text-white bg-red-500 ml-2 rounded-lg p-5">Delete Profile</Button>
+            </div>
         </div>
     )
 }
